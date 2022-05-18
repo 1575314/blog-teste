56da79828a9e0357077c81df19a84247c157ad2f
ANGELA  GUACARAN <102700392+1575314@users.noreply.github.com>
2022-05-18 16:48:36 -0300

    Add files via upload

---
+++ my/myblogpessoal/src/main/java/com/generation/myblogpessoal/model/Tema.java
@@ -0,0 +1,10 @@
+package com.generation.myblogpessoal.model;
+
+public class Tema {
+
+	public Long getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ my/myblogpessoal/src/test/java/com/generation/myblogpessal/MyblogpessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.myblogpessal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogpessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
