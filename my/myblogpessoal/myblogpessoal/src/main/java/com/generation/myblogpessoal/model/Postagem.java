384f9b102e42c4b96206a36f84116346236ef365
ANGELA  GUACARAN <102700392+1575314@users.noreply.github.com>
2022-05-18 21:57:02 -0300

    Add files via upload

---
+++ my/myblogpessoal/myblogpessoal/src/main/java/com/generation/myblogpessoal/model/Postagem.java
@@ -0,0 +1,17 @@
+package com.generation.myblogpessoal.model;
+
+public class Postagem {
+
+	public Object getTema() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Long getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	
+}
